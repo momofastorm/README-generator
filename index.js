436042a9e0cb2ad4b9a19bac9a4aa63a2c7922a4
momofastorm <marcyrappaport.blessed@gmail.com>
2023-05-16 19:02:28 -0500

    added to index.js

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//project: title, description, usage, link, installation instructions, author, questions email, screen shot
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'Project title?',
+}
+{
+    type: 'input',
+    name: 'description',
+    message: 'Project description?',
+}
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Project usage',
+}
+{
+    type: 'input',
+    name: 'link',
+    message: 'Project link?',
+}
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Installation instructions?',
+}
+{
+    type: 'author',
+    name: 'author',
+    message: 'Project author(s)?',
+}
+{
+    type: 'questions',
+    name: 'questions',
+    message: 'Questions please email?',
+}
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
